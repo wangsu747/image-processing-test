812bda004a2bf29fb89a809ddd1bb9e015e3fba3
SueSue <306496273@qq.com>
2023-08-30 14:29:05 -0600

    Initial commit

--- pytorch_classification/Test7_shufflenet/train.py
+++ pytorch_classification/Test7_shufflenet/train.py
@@ -111,9 +111,9 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_classes', type=int, default=27)
+    parser.add_argument('--num_classes', type=int, default=25)
     parser.add_argument('--epochs', type=int, default=30)
-    parser.add_argument('--batch-size', type=int, default=16)
+    parser.add_argument('--batch-size', type=int, default=2)
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--lrf', type=float, default=0.1)
 

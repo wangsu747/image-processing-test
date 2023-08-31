d123bb4e649481ad14df1cc05bfc736f282037ea
SueSue <306496273@qq.com>
2023-08-31 16:02:04 -0600

    Initial commit

--- pytorch_classification/Test7_shufflenet/train.py
+++ pytorch_classification/Test7_shufflenet/train.py
@@ -27,10 +27,12 @@ def main(args):
     data_transform = {
         "train": transforms.Compose([transforms.RandomResizedCrop(224),
                                      transforms.RandomHorizontalFlip(),
+                                     transforms.Grayscale(num_output_channels=1),
                                      transforms.ToTensor(),
                                      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
         "val": transforms.Compose([transforms.Resize(256),
                                    transforms.CenterCrop(224),
+                                   transforms.Grayscale(num_output_channels=1),
                                    transforms.ToTensor(),
                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
 

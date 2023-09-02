c789d29dd212b23fa6b1d0e7573d6a34b30e7219
SueSue <306496273@qq.com>
2023-09-02 15:48:03 -0600

    Initial commit

--- pytorch_classification/Test7_shufflenet/predict_normal.py
+++ pytorch_classification/Test7_shufflenet/predict_normal.py
@@ -18,15 +18,12 @@ def main(args):
          transforms.ToTensor(),
          transforms.Normalize([0.485, ], [0.229,])])
 
-    # load image
-    img_path = args.data_path
-    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
-    img = Image.open(img_path)
-    plt.imshow(img)
-    # [N, C, H, W]
-    img = data_transform(img)
-    # expand batch dimension
-    img = torch.unsqueeze(img, dim=0)
+    # create model
+    model = shufflenet_v2_x1_0(num_classes=25).to(device)
+    # load model weights
+    model_weight_path = args.weights
+    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+    model.eval()
 
     # read class_indict
     json_path = './class_indices.json'
@@ -35,43 +32,59 @@ def main(args):
     with open(json_path, "r") as f:
         class_indict = json.load(f)
 
-    # create model
-    model = shufflenet_v2_x1_0(num_classes=5).to(device)
-    # load model weights
-    model_weight_path = args.weights
-    model.load_state_dict(torch.load(model_weight_path, map_location=device))
-    model.eval()
-    with torch.no_grad():
-        # predict class
-        output = torch.squeeze(model(img.to(device))).cpu()
-        predict = torch.softmax(output, dim=0)
-        predict_cla = torch.argmax(predict).numpy()
+    # 加载类别,label标签,一行一个
+    class_labels = []
+    with open("label.txt", "r") as f:
+        class_labels = [line.strip() for line in f.readlines()]
+    print('class_label = {}'.format(class_labels))
 
-    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
-                                                 predict[predict_cla].numpy())
-    plt.title(print_res)
-    for i in range(len(predict)):
-        print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
-                                                  predict[i].numpy()))
-    plt.show()
+    # load image
+    data_path = args.data_path
+    assert os.path.exists(data_path), "file: '{}' dose not exist.".format(data_path)
+    i = -1
+    correct = 0
+    for filename in os.listdir(data_path):
+        i += 1
+        img_path = os.path.join(data_path,filename)
+        img = Image.open(img_path)
+        img = img.convert('L')
+        # plt.imshow(img)
+    # [N, C, H, W]
+        img = data_transform(img)
+    # expand batch dimension
+        img = torch.unsqueeze(img, dim=0)
+        with torch.no_grad():
+            # predict class
+            output = torch.squeeze(model(img.to(device))).cpu()
+            predict = torch.softmax(output, dim=0)
+            predict_cla = torch.argmax(predict).numpy()
+            # print('predict_cla = {}'.format(type(predict_cla)))
+            # print('class_labels[i] = {}'.format(type(class_labels[i])))
+            # print('equal or not = {}'.format(str(predict_cla) == class_labels[i]))
+            if str(predict_cla) == class_labels[i]:
+                correct += 1
+
+    print('class: {}   accurate : {:.4}'.format(class_indict[str(class_labels[0])], correct/(i+1)))
+
+            # print('predict_cla = {}'.format(predict_cla))
+
+
+
+
+
+    # print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
+    #                                              predict[predict_cla].numpy())
+    # plt.title(print_res)
+    # for i in range(len(predict)):
+    #     print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
+    #                                               predict[i].numpy()))
+    # plt.show()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--num_classes', type=int, default=25)
-    # parser.add_argument('--epochs', type=int, default=30)
-    # parser.add_argument('--batch-size', type=int, default=2)
-    # parser.add_argument('--lr', type=float, default=0.01)
-    # parser.add_argument('--lrf', type=float, default=0.1)
-
-
     parser.add_argument('--data-path', type=str,
                         default="/data/flower_photos")
-
-    # shufflenetv2_x1.0 官方权重下载地址
-    # https://download.pytorch.org/models/shufflenetv2_x1-5666bf0f80.pth
-    # parser.add_argument('--weights', type=str, default='./shufflenetv2_x1.pth',
-    #                     help='initial weights path')
     parser.add_argument('--weights', type=str, default='',
                         help='initial weights path')
     # parser.add_argument('--freeze-layers', type=bool, default=False)
